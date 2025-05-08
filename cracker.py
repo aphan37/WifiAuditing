980f78e079d73ace3bbf53eb3bb33cc43b6042b8
dmarshall00 <mdawn652@gmail.com>
2025-05-08 11:47:24 -0400

    Store output log after decoding

--- cracker.py
+++ cracker.py
@@ -1,6 +1,10 @@
 # === wifi_auditor/cracker.py ===
 import subprocess
-def crack_handshake(target_bssid, handshake_file, wordlist_file):
+def crack_handshake(target_bssid, handshake_file, wordlist_file, log_file="aircrack_out.log"):
     print("[*] Cracking handshake with Aircrack-ng...")
     cmd = ["aircrack-ng", "-w", wordlist_file, "-b", target_bssid, handshake_file]
-    subprocess.run(cmd)
+
+    with open(log_file, "w") as f:
+        subprocess.run(cmd, stdout=f, stderr=subprocess.STDOUT)
+
+    print(f"[*] Output saved to {log_file}")    
